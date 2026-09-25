836b994c7bb9a70bb57a23482cb2eb3f54463904
Hiba THOUM <thoumhiba2@gmail.com>
2026-09-25 19:11:32 +0000

    Mise a jour de SITE_URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
 const CONFIG = {
-  SITE_URL: "https://yom-emsi.netlify.app",
+  SITE_URL: "https://qr-code-iota-smoky-64.vercel.app/",
   GOOGLE_FORM_URL: "https://forms.gle/9cYM17z4B9QzgjEq5"
 };
